99863becf1a813b3825b1892c19ffbde06f4be64
Fabian Guerrero <lnava@salesforce.com>
2016-12-07 13:01:24 -0800

    Initial setup :punch:

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace slaveMonitor
+{
+    partial class SlaveMonitor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SlaveMonitor));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Here to wake up jenkins slave";
+            // 
+            // SlaveMonitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 47);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SlaveMonitor";
+            this.Text = "SlaveMonitor";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Automation;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace slaveMonitor
+{
+    public enum ErrorType
+    {
+        NoError = 0,
+        SecurityCheckError = 1,
+        ConnectionError = 2,
+        SlaveAgentNotLaunched = 3,
+        SlaveAgentTerminated = 4
+    }
+
+    public partial class SlaveMonitor : Form
+    {
+        const string ExpectedProcessName = "jp2launcher";
+        const string SlaveMonitorProcessName = "slaveMonitor";
+        const string Command = "javaws";
+        const string ScriptName = @"-Xnosplash .\cloudbees-agent.jnlp";
+        private TimeSpan pollInverval = TimeSpan.FromMinutes(10);
+        private TimeSpan inBetweenDialogsInterval = TimeSpan.FromSeconds(5);
+        private Thread workerThread;
+        
+
+        public SlaveMonitor()
+        {
+            InitializeComponent();
+            Show();
+            KillPrevSlaveMonitor();
+
+            workerThread = new Thread(new System.Threading.ThreadStart(Loop));
+            workerThread.Start();
+        }
+
+        public bool IsHostProcessAlive()
+        {
+            label1.Text = "Checking...";
+            int? handleCount = 0;
+            var processList = Process.GetProcesses();
+            var targetProcess =
+                (processList.Where(process => process.ProcessName.Equals(ExpectedProcessName))).FirstOrDefault();
+            if (targetProcess != null)
+            {
+                handleCount = targetProcess?.HandleCount;
+                if (handleCount != 22)   //Failing case had 20 User Handles
+                    throw new ApplicationException();
+                else
+                    return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public void LaunchJenkinsApplet()
+        {
+            label1.Text = "Launching Jenkins slave agent...";
+            var processInfo = new ProcessStartInfo();
+            processInfo.FileName = Command;
+            processInfo.Arguments = ScriptName;
+            Debug.WriteLine("Running command: {0} {1}", processInfo.FileName, processInfo.Arguments);
+            var p = Process.Start(processInfo);
+            Debug.WriteLine(p.HandleCount);
+        }
+
+        public void KillAgent()
+        {
+            var processList = Process.GetProcesses();
+            var targetProcess =
+                (processList.Where(process => process.ProcessName.Equals(ExpectedProcessName))).FirstOrDefault();
+            targetProcess?.Kill();
+        }
+
+        public void KillPrevSlaveMonitor()
+        {
+            //Kill all previous slaveMonitor processes but current one
+            var currentProcessId = Process.GetCurrentProcess().Id;
+            var processList = Process.GetProcesses();
+            var processes = (processList.Where(process => process.ProcessName.Equals(SlaveMonitorProcessName) && process.Id != currentProcessId));
+
+            foreach (var process in processes)
+            {
+                process.Kill();
+            }
+        }
+
+        public ErrorType CheckErrors()
+        {
+            Debug.WriteLine("Looking for Security dialog");
+
+            var propCondition = new AndCondition(new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Window) ,new PropertyCondition(AutomationElement.NameProperty, "Security Warning"));
+            var servletWnd = AutomationElement.RootElement.FindFirst(TreeScope.Children, propCondition);
+            if (servletWnd != null)
+                return ErrorType.SecurityCheckError;
+
+            Debug.WriteLine("Looking Slave agent Window");
+            propCondition = new AndCondition(new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Window), new PropertyCondition(AutomationElement.NameProperty, "Jenkins slave agent"));
+            var slaveWindow = AutomationElement.RootElement.FindFirst(TreeScope.Children, propCondition);
+
+            if (slaveWindow == null)
+                return ErrorType.SlaveAgentNotLaunched;
+
+            Debug.WriteLine("Looking for connection error Window");
+            propCondition = new AndCondition(new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Window), new PropertyCondition(AutomationElement.NameProperty, "Error"));
+            var errorDialog = slaveWindow.FindFirst(TreeScope.Children, propCondition);
+
+            if(errorDialog != null)
+                return ErrorType.ConnectionError;
+
+            return ErrorType.NoError;
+        }
+
+        public void Loop()
+        {
+            while (true)
+            {
+                try
+                {
+                    if (!IsHostProcessAlive())
+                    {
+                        Debug.WriteLine("Process not found, running applet");
+                        LaunchJenkinsApplet();
+                        System.Threading.Thread.Sleep(inBetweenDialogsInterval);
+                        var executionError = CheckErrors();
+                        if (executionError != ErrorType.NoError)
+                        {
+                            Debug.WriteLine($"ErrorType {executionError}: Human intervention required!!");
+                            if(executionError != ErrorType.SlaveAgentNotLaunched)
+                                KillAgent();
+                            break;
+                        }
+                        else
+                        {
+                            label1.Text = "He's awake!!!";
+                        }
+                    }
+
+                    System.Threading.Thread.Sleep(pollInverval);
+                }
+                catch (ApplicationException)
+                {
+                    throw;
+                }
+            }
+
+            this.Close();
+        }
+    }
+}
